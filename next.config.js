f13ffc3b90ea1f40fbdfe698af33e76b664b8452
yurii_fishbakh <bunbanto@gmail.com>
2026-01-23 01:36:10 +0100

    add prod serv api

--- next.config.js
+++ next.config.js
@@ -2,10 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
+    // Використовуємо змінну середовища для API URL
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000";
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:4000/:path*",
+        destination: `${apiUrl}/:path*`,
       },
     ];
   },
